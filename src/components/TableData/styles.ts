265aca4128c0857e196de76c8302dac1255ab437
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-26 23:10:03 -0300

    🎨 update TableData

--- src/components/TableData/styles.ts
+++ src/components/TableData/styles.ts
@@ -1,23 +1,25 @@
 import { styled, theme } from "../../styles/stitches.config";
 
-export const TableContainer = styled('div', {
+export const TableContainer = styled('table', {
   width: '100%',
   border: `1px solid ${theme.colors.brownDark700}`,
   borderRadius: '10px',
   overflowX: 'auto',
+  display: 'block',
 });
 
-export const TableHead = styled('div', {});
+export const TableHead = styled('thead', {});
 
-export const TableBody = styled('div', {});
+export const TableBody = styled('tbody', {});
 
-export const TableTr = styled('div', {
+export const TableTr = styled('tr', {
   display: 'grid',
   gridTemplateColumns: 'repeat(6, 1fr)',
   paddingInline: '1.5rem',
   paddingBlock: '0.75rem',
   width: 'max-content',
   minWidth: '100%',
+  gap: '1rem',
 
   variants: {
     hasBorderBottom: {
@@ -34,7 +36,7 @@ export const TableTr = styled('div', {
   }
 });
 
-export const TableTh = styled('div', {
+export const TableTh = styled('th', {
   minWidth: '128px',
   fontWeight: '600',
   fontSize: '$13',
@@ -42,7 +44,7 @@ export const TableTh = styled('div', {
   textAlign: 'start',
 });
 
-export const TableTd = styled('div', {
+export const TableTd = styled('td', {
   fontSize: '$12',
   color: '$brownDark700',
   minWidth: '128px',
